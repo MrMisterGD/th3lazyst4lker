e73bf3fbac0b8c059edc1d0aa9dab1cf662ab073
MrMisterGD <mrmistergd@protonmail.com>
2026-04-20 00:21:13 -0400

    versioning update

--- st4lker/__init__.py
+++ st4lker/__init__.py
@@ -1,7 +1,7 @@
 """
 st4lker - The OSINT tool for lazy people
-version 1.1.0 - now with more tools and more headaches
+version 1.1.1 - now with more tools and more headaches
 """
 
-__version__ = "1.1.0"
+__version__ = "v1.1.1"
 __author__ = "Mr. Mister GD"
